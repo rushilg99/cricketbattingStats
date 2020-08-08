59824fbef83f89eadf555cf875a49074ba72c4c7
rushilg99 <rushil1.gholkar@gmail.com>
2020-08-08 22:01:56 +0100

    New code extracts the playing role of each cricketer (if it exists)

--- scrape.py
+++ scrape.py
@@ -4,19 +4,8 @@ import csv
 
 # Extract data from webpage and find the table in which all necessary data is required
 def extract():
-    '''website = requests.get("https://stats.espncricinfo.com/ci/engine/stats/index.html?class=1;spanmin1=01+Jan+1986;spanval1=span;template=results;type=batting")
-    soup = BeautifulSoup(website.content, 'lxml')
-    contenttable = soup.findAll('table', attrs={'class':'engineTable'})
-    data = contenttable[2] # There are 3 different engineTables on the webpage, the latter being the one I want
-    # Extract table headers
-    headers = (data.find('tr', attrs={'class':'headlinks'})).text.strip().split("\n")
-    playerstats = []
-    # For each piece of player data, strip to remove trailing and leading whitespace and turn into an array.
-    # Then add to playerstats[]
-    for i in data.findAll('tr', attrs={'class':'data1'}):
-        playerstats.append((i.text.strip()).split("\n"))'''
     i = 1 # page 1
-    playerstats = [] # array containing each player record
+    playerstats = [] # list containing each player record
     ''' we loop through each page that has data. Break only when no more data available'''
     while True: 
         website = requests.get("https://stats.espncricinfo.com/ci/engine/stats/index.html?class=1;page="+str(i)+";spanmin1=01+Jan+1986;spanval1=span;template=results;type=batting")
@@ -30,16 +19,17 @@ def extract():
             # Headers located in <tr> tag with class headlinks. Remove leading \n's, split into headers array
             headers = (data.find('tr', attrs={'class':'headlinks'})).text.strip().split("\n")
             headers.append("Batting Style")
+            headers.append("Player Role")
         if data.text.find("No records available to match this query") == -1:
             # In this case, we have data available on the page
             # For each piece of player data, strip to remove trailing and leading whitespace and turn into an array
             # Then add to playerstats[]
             for j in data.findAll('tr', attrs={'class':'data1'}):
-                link = j.find('a', attrs={'class':'data-link'})
-                info = (j.text.strip()).split("\n")
-                playerpage = requests.get("https://stats.espncricinfo.com" + str(link.get("href")))
+                info = (j.text.strip()).split("\n") # Gets player info as an array
+                link = j.find("a", attrs={"class":"data-link"}) # Gets the latter part of the link to the player page
+                playerpage = requests.get("https://stats.espncricinfo.com" + str(link.get("href"))) # Connects to the individual player page
                 soup2 = BeautifulSoup(playerpage.content, 'lxml')
-                playerbatstyle = soup2.find('b', string="Batting style").find_parent("p").find("span").text
+                playerbatstyle = soup2.find('b', string="Batting style").find_parent("p").find("span").text # Finds out whether or not a player is left or right handed
                 try:
                     lhrs = playerbatstyle.lower()
                     if lhrs.find("right") != -1:
@@ -48,7 +38,14 @@ def extract():
                         info.append("L")
                 except IndexError:
                     info.append("-")
-                    print(info[0])
+
+                # Now get the player's role in the team
+                playerRole = soup2.find("b", string="Playing role")
+                if playerRole != None:
+                    playerRole = playerRole.find_parent("p").find("span").text
+                    info.append(playerRole.lower())
+                else:
+                    info.append("")
                 print(info[0])
                 playerstats.append(info)
         else:
